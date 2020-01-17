ec4c1b8f77e4d979e98990f39ddacbb191b7284d
gokhj <gokhanarkan@gmail.com>
2020-01-17 10:32:39 +0000

    config.py skeleton added

---
+++ app/functions/instance/config.py
@@ -0,0 +1,18 @@
+heroku_redis = 'HEROKU REDIS SERVICE'
+redis_to_go = 'REDIS TO GO SERVICE'
+service_check_url = "FIRST API"
+service_check_headers = {
+    'api-host': "FIRST API HOST",
+    'api-key': "FIRST API KEY"
+}
+netflix_url = "SECOND API"
+netflix_headers = {
+    'api-host': "SECOND API HOST",
+    'api-key': "SECOND API KEY"
+}
+country_url = "THIRD API"
+country_headers = {
+    'api-host': "THIRD API HOST",
+    'api-key': "THIRD API KEY"
+}
+apikey = 'API KEY TO FLUSH CACHES'
