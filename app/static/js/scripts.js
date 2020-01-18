76564551dac11f2dea99ad1e49ce2dffc5a2c97a
gokhj <gokhanarkan@gmail.com>
2020-01-18 18:06:20 +0000

    Change country post request on js

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,9 +1,71 @@
 function submit(id) {
-    $.post("/get_country_list", {"netflixid": id}, function(data){
+    $.post("/get_country_list", { "netflixid": id }, function (data) {
         $("#modal-body").html(data);
         $("#countryModal").modal();
     });
 }
+
+function change_country(country, country_name) {
+    $.post("/change_country", { "country": country }, function (data) {
+        // Creating array out of the data
+        let result = JSON.parse(data);
+        final_result = create_results(result);
+        $("#original-results").hide()
+        $("#ajax-results").html(final_result);
+        $("#country-results").html("Here are the results for " + country_name);
+    })
+}
+
+function create_results(result) {
+
+    final_result = "";
+    for (let i = 0; i < result.length; i++) {
+
+        obj = result[i];
+
+        netflixid = obj.netflixid;
+
+        if (obj.large_image) {
+            image = obj.large_image;
+        } else {
+            image = obj.image;
+        }
+        title = obj.title;
+        release_date = obj.release_date;
+        rating = obj.rating;
+        time = obj.time;
+        synopsis = obj.synopsis;
+        link = obj.link;
+
+        // let a = `<h1>this is a test ${netflixid}</h1>`;
+        let first_part =
+            `<div class="card mb-3 fliks-results" style="margin-bottom: 0.3rem !important;">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                    <img src="${image}" class="card-img" alt="${title}">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">${title}  (${release_date})</h5>
+                        <h6 class="card-subtitle mb-2 text-muted" style="margin-bottom: 0"> Rating: ${rating} Time: ${time}
+                        </h6>
+                        <p class="card-text" style="margin-bottom:0">${synopsis}</p>
+                        <a href="${link}" target="_blank" class="btn btn-link">Watch</a>
+                        <button class="btn btn-link" onclick="submit(${netflixid})">Country details</button>`
+
+        let closing_tags = `</div></div></div></div>\n`
+        if (obj.download == 1) {
+            let downloadble = `<p class="card-text"><small class="text-muted">This content is downloadable</small></p>`
+            final_result += first_part + downloadble + closing_tags;
+        } else {
+            final_result += first_part + closing_tags;
+        }
+    }
+
+    return final_result
+
+}
+
 
 $(document)
     .ajaxStart(function () {
